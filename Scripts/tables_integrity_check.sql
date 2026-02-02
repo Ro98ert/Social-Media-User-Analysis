cff2d469d423ef5e8172df6259742779c5c17aed
Robert Amartisoaei <robert.amartisoaei@hotmail.com>
2026-02-02 17:57:38 +0200

    Update file with the script

--- Scripts/tables_integrity_check.sql
+++ Scripts/tables_integrity_check.sql
@@ -1,1 +1,12 @@
-
+# I check the integrity of the tables ensuring it uploaded correctly. Observing that there are ~1.55mil users instead of 1 million listed in the dataset's description.
+SELECT
+  'usage_table' AS table_name,
+  COUNT(*) AS total_rows,
+  COUNT(DISTINCT User_ID) AS unique_users
+FROM `social-media-analysis-485720.social_media_dataset_raw.instagram_usage_lifestyle_raw`
+UNION ALL
+SELECT
+  'users_table' AS table_name,
+  COUNT(*) AS total_rows,
+  COUNT(DISTINCT User_ID) AS unique_users
+FROM `social-media-analysis-485720.social_media_dataset_raw.instagram_users_lifestyle_raw`
